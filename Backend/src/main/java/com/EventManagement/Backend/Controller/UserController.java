76a88f764beb43878551a9bf9141526075e0cd19
sujal8923 <sujalroy1822@gmail.com>
2025-07-07 11:46:52 +0530

    something

--- Backend/src/main/java/com/EventManagement/Backend/Controller/UserController.java
+++ Backend/src/main/java/com/EventManagement/Backend/Controller/UserController.java
@@ -15,7 +15,7 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/user")
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = "http://localhost:5173",allowCredentials = "true")
 public class UserController {
     @Autowired
     private EventService eventService;
