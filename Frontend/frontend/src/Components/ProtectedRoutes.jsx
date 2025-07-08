03bc5398f260c96ca00524b37317826c3a3ccba3
sujal8923 <sujalroy1822@gmail.com>
2025-07-08 17:02:37 +0530

    test

---
+++ Frontend/frontend/src/Components/ProtectedRoutes.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function ProtectedRoutes({allowedRoles, children}) {
+    // const userRole = localStorage.getItem('userRole');
+    // if
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default ProtectedRoutes
